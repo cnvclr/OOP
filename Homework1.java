7f9c297098bcb8a9083773cee1ddd263674e5d78
cnvclr <164980014+cnvclr@users.noreply.github.com>
2025-09-11 14:09:40 +0900

    Add files via upload

---
+++ Homework1.java
@@ -0,0 +1,17 @@
+package smu._0911;
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i=0; i<5; i++) {
+            System.out.println("정수를 입력하세요: ");
+            int num = sc.nextInt();
+            sum += num;
+            System.out.println("현재까지 입력된 정수의 합은 " + sum + "입니다.\n");
+        }
+        sc.close();
+    }
+}
