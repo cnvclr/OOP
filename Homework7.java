5889777ce1ef2e67321180a5e424599318707a91
cnvclr <164980014+cnvclr@users.noreply.github.com>
2025-11-16 23:18:18 +0900

    Add files via upload

---
+++ Homework7.java
@@ -0,0 +1,56 @@
+package smu.Homework;
+
+abstract class Printer {
+    private String model;
+    protected int printedCount;
+    protected int availableCount;
+
+    public Printer(String model, int availableCount) {
+        this.model = model;
+        this.printedCount = 0;
+        this.availableCount = availableCount;
+    }
+
+    public String getModel() { return model; }
+
+    public abstract boolean print();
+}
+
+class InkjetPrinter extends Printer {
+    public InkjetPrinter(String model, int availableCount) { super(model, availableCount); }
+
+    @Override
+    public boolean print() {
+        if (availableCount == 0) {
+            System.out.printf("%s: %d매째 인쇄 실패 - 잉크 부족.\n", getModel(), printedCount + 1);
+            return false;
+        } else {
+            printedCount++; availableCount--;
+            return true;
+        }
+    }
+}
+
+class LaserPrinter extends Printer {
+    public LaserPrinter(String model, int availableCount) { super(model, availableCount); }
+
+    @Override
+    public boolean print() {
+        if (availableCount == 0) {
+            System.out.printf("%s: %d매째 인쇄 실패 - 토너 부족.\n", getModel(), printedCount + 1);
+            return false;
+        } else {
+            printedCount++; availableCount--;
+            return true;
+        }
+    }
+}
+
+public class Homework7 {
+    public static void main(String[] args) {
+        InkjetPrinter inkjet = new InkjetPrinter("Brother DCP-T730DW", 7500);
+        LaserPrinter laser = new LaserPrinter("Canon MFC643Cdw", 1500);
+        while (inkjet.print());
+        while (laser.print());
+    }
+}
